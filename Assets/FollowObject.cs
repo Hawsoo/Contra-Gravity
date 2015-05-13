c64c2f557f87ab042a55fea70cbe1fcb976ebf0d
Timothy Bennett <timoguy99004@gmail.com>
2015-05-13 16:59:04 -0700

    Creates movement system (basic)

---
+++ Assets/FollowObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour
+{
+    public GameObject obj;
+
+	// Update
+	void Update ()
+    {
+        obj.transform.position = new Vector3(transform.position.x, obj.transform.position.y, obj.transform.position.z);
+	}
+}
---
+++ Assets/ModifyGravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifyGravity : MonoBehaviour
+{
+    public float gravDirOffset;
+
+    // Switch gravity
+    void OnTriggerEnter(Collider other)
+    {
+        // If player entered
+        if (other.gameObject.tag == "Player")
+        {
+            // Switch player's gravity
+            PlayerMovement.gravDir = transform.rotation.eulerAngles.z + gravDirOffset;
+        }
+    }
+}

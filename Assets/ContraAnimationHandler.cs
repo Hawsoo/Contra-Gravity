8f52f88c794a9daf1f56d19f59b397d67c2c7d3e
Hawsoo <timoguy99004@gmail.com>
2015-05-22 05:38:43 -0700

    Adds triple combo attack

--- Assets/ContraAnimationHandler.cs
+++ Assets/ContraAnimationHandler.cs
@@ -1,30 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum HornPosition
+{
+    UP, CARRY
+}
+
 public class ContraAnimationHandler : MonoBehaviour
 {
-    private bool nextAttack = false;
+    private bool xHeld = false;
+    private bool spaceHeld = false;
+
+    private HornPosition hornpos = HornPosition.CARRY;
+
+    private bool attack2 = false;
+    private bool attack3 = false;
 
     // Update
     public void AniUpdate()
     {
-        // Attack
-        if (Input.GetKey(KeyCode.X))
+        // Toggle horn position
+        if (Input.GetKey(KeyCode.Space) && !spaceHeld)
         {
-            if (nextAttack)
+            if (hornpos == HornPosition.CARRY)
             {
-                nextAttack = false;
-                GetComponent<Animator>().Play("attack2");
+                hornpos = HornPosition.UP;
+
+                // Horns up
+                GetComponent<Animator>().Play("horn_up");
             }
-            else
+            else if (hornpos == HornPosition.UP)
             {
-                GetComponent<Animator>().Play("attack1");
+                hornpos = HornPosition.CARRY;
+
+                // Horns down
+                GetComponent<Animator>().Play("horn_down");
             }
         }
 
+        if (hornpos == HornPosition.CARRY)
+        {
+            // Attack
+            if (Input.GetKey(KeyCode.X) && !xHeld)
+            {
+                if (attack3)
+                {
+                    attack3 = false;
+                    GetComponent<Animator>().Play("attack3");
+                }
+                else if (attack2)
+                {
+                    attack2 = false;
+                    GetComponent<Animator>().Play("attack2");
+                }
+                else
+                {
+                    GetComponent<Animator>().Play("attack1");
+                }
+            }
+        }
+
+        // Update lagging messages
+        xHeld = Input.GetKey(KeyCode.X);
+        spaceHeld = Input.GetKey(KeyCode.Space);
     }
 
     // Messages
-    void AllowNextAttack() { nextAttack = true; Debug.Log("yes"); }
-    void DisallowNextAttack() { nextAttack = false; Debug.Log("non"); }
+    void AllowAttack2() { attack2 = true; }
+    void DisallowAttack2() { attack2 = false; }
+    void AllowAttack3() { attack3 = true; }
+    void DisallowAttack3() { attack3 = false; }
 }

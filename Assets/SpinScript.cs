87691745ae97ff77709d9214c864c42372f0f763
Timothy Bennett <timoguy99004@gmail.com>
2015-06-04 10:19:07 -0700

    Starts spikes; adds planets

---
+++ Assets/SpinScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinScript : MonoBehaviour
+{
+    public bool spin = true;
+    public float speed;
+
+    private Vector3 currentPos;
+
+    // Init
+    void Start()
+    {
+        currentPos = transform.position;
+    }
+
+    // Update
+    void FixedUpdate()
+    {
+        // Flag out
+        if (!spin) return;
+
+        transform.position = currentPos;
+
+        // Spin
+        GetComponent<Rigidbody2D>().angularVelocity = speed;
+    }
+}
